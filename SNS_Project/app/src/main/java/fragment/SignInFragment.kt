8351652432f1b2e88abfc97390b195307cd1afaf
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-16 09:15:09 +0900

    UI Login Layout 제작 완료 !

---
+++ SNS_Project/app/src/main/java/fragment/SignInFragment.kt
@@ -0,0 +1,20 @@
+package fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sns_project.R
+
+class SignInFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.activity_sign_in_fragment, container, false)
+        return view
+    }
+}
