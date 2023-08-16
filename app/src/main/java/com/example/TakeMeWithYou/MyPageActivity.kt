117235325beddb17e550a8b933c5742d2269b2ff
june <dydrkfl078@naver.com>
2023-08-16 14:53:37 +0900

    1st commit

--- app/src/main/java/com/example/TakeMeWithYou/MyPageActivity.kt
+++ app/src/main/java/com/example/TakeMeWithYou/MyPageActivity.kt
@@ -2,10 +2,15 @@ package com.example.TakeMeWithYou
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
 
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.mypage_activity)
+        setContentView(R.layout.activity_main)
+
+
     }
 }
+
+
