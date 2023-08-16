2c2a53a5fd1935cdbbf44c70f5a48626b58730d5
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-08-16 17:29:32 +0900

    add settings tool bar action button

--- app/src/main/java/com/example/TakeMeWithYou/SettingActivity.kt
+++ app/src/main/java/com/example/TakeMeWithYou/SettingActivity.kt
@@ -2,10 +2,18 @@ package com.example.TakeMeWithYou
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class SettingActivity : AppCompatActivity() {
+
+    private val settingToolbar: Toolbar by lazy { findViewById(R.id.setting_toolbar) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting_activity)
+//        settingToolbar = findViewById(R.id.setting_toolbar)
+        setSupportActionBar(settingToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
     }
 }
