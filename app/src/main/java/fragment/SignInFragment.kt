12d8415bcd2be42eb2d55a0623c25922a12358d7
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-17 13:58:12 +0900

    login & signup - change the id of editText & button

--- app/src/main/java/fragment/SignInFragment.kt
+++ app/src/main/java/fragment/SignInFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.TakeMeWithYou.R
 
@@ -14,6 +16,7 @@ class SignInFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.sign_in_fragment, container, false)
+
         return view
     }
 }
