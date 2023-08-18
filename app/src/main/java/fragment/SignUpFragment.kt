172fb6126f84babac3ac68d386c37e1ac29f2246
kkomin <100822055+kkomin@users.noreply.github.com>
2023-08-18 16:01:24 +0900

    [FEAT] : ⚙️   SignUpFragment - 입력에 따른 editText 활성화 및 비활성화 설정

--- app/src/main/java/fragment/SignUpFragment.kt
+++ app/src/main/java/fragment/SignUpFragment.kt
@@ -3,7 +3,9 @@ package fragment
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.text.InputType
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -33,6 +35,37 @@ class SignUpFragment : Fragment() {
         // pw는 영어, 숫자 및 특수문자만 입력 가능
         pw_edit.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
         // userId는 한/영, 숫자, 특수문자 모두 입력 가능 -> inputType 설정 필요 X
+
+        // id 입력 전 pw, userId editText 입력 불가
+        pw_edit.isEnabled = false
+        userId_edit.isEnabled = false
+
+        // id 입력 시 userId editText 활성화
+        id_edit.addTextChangedListener(object: TextWatcher {
+            // 텍스트 입력 후 호출
+            override fun afterTextChanged(s: Editable?) {
+                if(s?.isNotEmpty() == true)
+                    userId_edit.isEnabled = true
+            }
+            // 텍스트 변경 전 호출
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            // 텍스트 변경 중 호출
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        // id -> userId 입력시 pw editText 활성화
+        userId_edit.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if(s?.isNotEmpty() == true)
+                    pw_edit.isEnabled = true
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
 
         signUp.setOnClickListener {
             val id = id_edit.text.toString()
