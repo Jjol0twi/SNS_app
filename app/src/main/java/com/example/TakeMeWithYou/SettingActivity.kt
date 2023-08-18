26bdcd3d3f7271ddf6c6ae4698d9816057adb907
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-08-18 20:52:36 +0900

    add Listview item

--- app/src/main/java/com/example/TakeMeWithYou/SettingActivity.kt
+++ app/src/main/java/com/example/TakeMeWithYou/SettingActivity.kt
@@ -8,6 +8,12 @@ import androidx.appcompat.widget.Toolbar
 class SettingActivity : AppCompatActivity() {
 
     private val settingToolbar: Toolbar by lazy { findViewById(R.id.setting_toolbar) }
+    private val settingMainItems: Array<String> = arrayOf("설정", "폰트", "테마", "로그아웃")
+    private val settingSubItems: Map<String, Array<String>> = mapOf(
+        "설정" to arrayOf("시스템 설정", "한국어", "English"),
+        "폰트" to arrayOf("기본", "아직 준비중"),
+        "테마" to arrayOf("기본", "준비중"),
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting_activity)
