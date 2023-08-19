0572dc8e7da361e4da5e609768fff06c48e20392
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-08-20 05:03:50 +0900

    [FEAT] : ⚙️ 싱글턴 적욤 및 추가 fucntion 구현

--- app/src/main/java/com/example/TakeMeWithYou/data/UserList.kt
+++ app/src/main/java/com/example/TakeMeWithYou/data/UserList.kt
@@ -5,8 +5,40 @@ import com.example.TakeMeWithYou.model.User
 class UserList {
     var userList = mutableListOf<User>()
 
+    companion object {  // 싱글턴
+        @Volatile
+        private var instance: UserList? = null
+        fun getInstance(): UserList { // 다 같은 interface를 사용하게 유도
+            return instance ?: synchronized(this) {
+                instance ?: UserList().also { instance = it }
+            }
+        }
+    }
+
     // 클래스 생성자 초기화
     init {
-        userList.add(User("", "", ""))
+        userList.add(User("admin", "admin", "admin"))
     }
+
+    fun addUser(user: User) {
+        userList.add(user)
+    }
+
+    fun getUserId(position: Int): String {
+        return userList[position].userID
+    }
+
+    fun getUserIds(): List<String> {
+        return userList.map { user -> user.userID }
+    }
+
+    fun getUserPWs(): List<String> {
+        return userList.map { user -> user.userID }
+    }
+
+    fun getUserPW(position: Int): String {
+        return userList[position].userPW
+    }
+
+
 }
