7cd0de23d9e6915c2bbc4d828946b0c32e167f51
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-08-20 20:04:41 +0900

    add finsh

--- app/src/main/java/com/example/TakeMeWithYou/Postpageadd.kt
+++ app/src/main/java/com/example/TakeMeWithYou/Postpageadd.kt
@@ -1,11 +1,15 @@
 package com.example.TakeMeWithYou
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class Postpageadd : AppCompatActivity() {
+    lateinit var postpageFinish : ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.postpage_activity)
+        postpageFinish=findViewById(R.id.imageButton)
+        postpageFinish.setOnClickListener { finish() }
     }
 }
