19a1af709ac6814accb05f2b0abdbd9aca6d18a0
iWantBigMan <skarndgus8899@gmail.com>
2023-08-20 17:29:29 +0900

    Use:toolBar setting Item, Add: finish()

--- app/src/main/java/com/example/TakeMeWithYou/MainPageActivity.kt
+++ app/src/main/java/com/example/TakeMeWithYou/MainPageActivity.kt
@@ -1,13 +1,19 @@
 package com.example.TakeMeWithYou
 
+import android.content.ClipData.Item
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.example.TakeMeWithYou.data.PostContentData
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -19,12 +25,15 @@ class MainPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         overridePendingTransition(R.anim.from_up_enter, R.anim.none)
         setContentView(R.layout.mainpage_activity)
+        setCustomToolbar(R.id.mainToolbar)
+
         mainContentListView = findViewById(R.id.main_content_listView)
         val bottomnavi = findViewById<BottomNavigationView>(R.id.mainPageBottomNavigationView)
         bottomnavi.setOnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.menu_detail -> {
                     startActivity(Intent(this, DetailPage::class.java))
+                    finish()
                     true
                 }
                 R.id.menu_main -> {
@@ -33,6 +42,7 @@ class MainPageActivity : AppCompatActivity() {
                 }
                 R.id.menu_myPage -> {
                     startActivity(Intent(this, MyPageActivity::class.java))
+                    finish()
                     true
                 }
                 else -> false
@@ -58,6 +68,34 @@ class MainPageActivity : AppCompatActivity() {
             mainContentListView.addView(itemView)
         }
     }
-}
+
+
+    // 메뉴 아이템 툴바에 집어 넣기
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_top_toolbar, menu)
+        return true
+    }
+
+    private fun setCustomToolbar(layout: Int) {
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(layout)
+        // 툴바를 액션바로 지정
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        // 액션바에서 앱 이름 보이지 않게 지정
+        actionBar?.setDisplayShowCustomEnabled(false)
+        toolbar.setOnMenuItemClickListener{item ->
+            if (item.itemId == R.id.toolbar_btn_setting){
+                val intent = Intent(this, SettingActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else{
+                false
+            }
+        }
+        }
+    }
+
 
 
