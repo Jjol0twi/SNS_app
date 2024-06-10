bb2a76804b434cba628aa4473f4a33fb3bce13b4
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2024-06-11 00:22:09 +0900

    add detail page fragment

---
+++ app/src/main/java/com/example/TakeMeWithYou/presentation/detail/DetailFragment.kt
@@ -0,0 +1,31 @@
+package com.example.TakeMeWithYou.presentation.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.TakeMeWithYou.databinding.DetailFragmentBinding
+
+class DetailFragment : Fragment() {
+    private var _binding: DetailFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DetailFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
